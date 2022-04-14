7cc4b3596ebd77fab5e9772ec8df5f8c0d9fb4ca
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-04-14 22:26:02 +0100

    Ex4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,6 +9,17 @@ const totalLikes = blogs => {
 	return blogs.map(b => b.likes).reduce((a, b) => a + b, 0)
 }
 
+const favouriteBlog = blogs => {
+	if (blogs.length === 0) return
+	const mostLikes = Math.max(...blogs.map(b => b.likes))
+	const favourite = blogs.filter(b => b.likes === mostLikes)[0]
+	return {
+		title: favourite.title,
+		author: favourite.author,
+		likes: favourite.likes
+	}
+}
+
 module.exports = {
-	dummy, totalLikes
+	dummy, totalLikes, favouriteBlog
 }
