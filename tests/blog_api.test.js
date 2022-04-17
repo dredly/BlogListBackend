999c58e692585425542d8e27dfbf858a0fbfc014
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-04-17 20:04:23 +0100

    Ex4.8

---
+++ tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const { blogs } = require('./dummy_data')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+	await Blog.deleteMany({})
+	const blogObjects = blogs.map(blog => new Blog(blog))
+	const promiseArray = blogObjects.map(blogObject => blogObject.save())
+	await Promise.all(promiseArray)
+})
+
+describe('getting all blogs', () => {
+	test('blogs are returned as JSON', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('all blogs are returned', async () => {
+		const response = await api.get('/api/blogs')
+		expect(response.body).toHaveLength(blogs.length)
+	})
+})
+
+afterAll(() => {
+	mongoose.connection.close()
+})
