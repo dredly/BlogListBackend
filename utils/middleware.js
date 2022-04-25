d3ed3e8a7488d6e06c57211e66005e9db84dfc78
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-04-25 14:08:36 +0100

    Ex4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -24,7 +24,16 @@ const errorHandler = (err, req, res, next) => {
 	next(err)
 }
 
+const tokenExtractor = (req, res, next) => {
+	const authorization = req.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		req.token = authorization.substring(7)
+	}
+	next()
+}
+
 module.exports = {
 	unknownEndpoint,
-	errorHandler
+	errorHandler,
+	tokenExtractor
 }
